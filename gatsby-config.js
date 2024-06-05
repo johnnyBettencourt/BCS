d41be9a1fdc2ad463486b6f7eee5b085e162d3f7
John Bettencourt <johnnybettencourt75@gmail.com>
2024-06-05 14:49:33 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `BettencourtCreativeSolutions`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
